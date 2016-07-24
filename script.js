52cb215fe6f2b9761a4ea39e118cdc10743b2ac0
Martti Dumangeng <bangwar@gmail.com>
2016-07-24 12:59:12 +0800

    Added more conversation logic

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ module.exports = new Script({
             const txt = message.text;
             switch (txt) {
                 case "postpic1":
+                    //put action here for postpic1 if there is any
                 case "seecake1":
                     return bot.say('![](https://raw.githubusercontent.com/esthercrawford/smooch-bot-example/master/img/esther.jpg)')
                         .then(() => 'morecake1');
@@ -29,8 +30,75 @@ module.exports = new Script({
     },
 
     morecake1: {
-        receive: (bot) => {
-            return bot.say('%[More](postback:more1) %[Something else](postback:somethingelse1)');
+        prompt: (bot) => bot.say('%[More](postback:more1) %[Something else](postback:somethingelse1)'),
+        receive: (bot, message) => {
+            const txt = message.text;
+            switch (txt) {
+                case "more1": //postback reply
+                    return bot.say('![](https://raw.githubusercontent.com/esthercrawford/smooch-bot-example/master/img/esther.jpg)')
+                        .then(() => 'morecake1');
+                case "somethingelse1": //postback reply
+                    return bot.say('OK. What do you want to do?')
+                        .then(() => 'something1');
+            }
+        }
+    },
+
+    something1: {
+        prompt: (bot) => bot.say('%[Post a comment](postback:postcomment1) %[Send us feedback](postback:sendfeedback1)'),
+        receive: (bot, message) => {
+            const txt = message.text;
+            switch (txt) {
+                case "postcomment1":
+                    //postcomment reply action here
+                case "sendfeedback1":
+                    return bot.say('Great! We’d love to hear from you!')
+                        .then(() => 'askName');
+            }
+        }
+    },
+
+    askName: {
+        prompt: (bot) => bot.say('What\'s your name?'),
+        receive: (bot, message) => {
+            const name = message.text;
+            return bot.setProp('name', name)
+                .then(() => bot.say(`Hi ${name}!`))
+                .then(() => 'askLocation');
+        }
+    },
+
+    askLocation: {
+        prompt: (bot) => bot.say('Where are you located?'),
+        receive: (bot, message) => {
+            const location = message.text;
+            return bot.setProp('location', location)
+                .then(() => bot.say(`Great! ${location} is wonderful!`))
+                .then(() => 'askFeedback');
+        }
+    },
+
+    askFeedback: {
+        prompt: (bot) => bot.say('What do you want to tell us?'),
+        receive: (bot, message) => {
+            const feedback = message.text;
+            return bot.setProp('feedback', feedback)
+                .then(() => bot.say(`Thanks for the feedback! I will share it with the admin`))
+                .then(() => 'showMorePhotos');
+        }
+    },
+
+    showMorePhotos: {
+        prompt: (bot) => bot.say('%[Yes, cake is awesome!](postback:awesome) %[No, I\'m good](postback:imgood)'),
+        receive: (bot, message) => {
+            const txt = message.text;
+            switch (txt) {
+                case "awesome":
+                    return bot.say('![](https://raw.githubusercontent.com/esthercrawford/smooch-bot-example/master/img/esther.jpg)')
+                        .then(() => 'morecake1');
+                case "imgood":
+                    //postcomment reply action here
+            }
         }
     },
 
