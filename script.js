d286c69ebcc840119b4b04b7c08dbca9103e76b8
Martti Dumangeng <bangwar@gmail.com>
2016-07-24 13:34:57 +0800

    no message

--- script.js
+++ script.js
@@ -65,8 +65,7 @@ module.exports = new Script({
             const txt = message.text;
             switch (txt) {
                 case "postcomment1":
-                    return bot.say('')
-                        .then(() => 'getComment');
+                    return bot.then(() => 'getComment');
                 case "sendfeedback1":
                     return bot.say('Great! We’d love to hear from you!')
                         .then(() => 'askName');
